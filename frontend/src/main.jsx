be825514fc20ef532da8b6d321f5036f70c546b0
sakethsenapathi <saketh7senapathi7@gmail.com>
2025-08-27 18:31:24 +1000

    viteconfig

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,10 +5,15 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import StoreContextProvider from './Context/StoreContext'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+function App() {
+ 
+   return ( 
+    <div>
+   <BrowserRouter>
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
-  </BrowserRouter>,
-)
+   </BrowserRouter>
+   </div>
+   )
+}
